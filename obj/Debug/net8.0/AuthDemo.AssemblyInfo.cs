8dc2eedd5f0f1bbe032b7242d7e639c1242a5eaa
hefkay <hefkay.lawal2019@gmail.com>
2024-06-02 10:08:59 -0700

    added bin and obj to be ignored

--- obj/Debug/net8.0/AuthDemo.AssemblyInfo.cs
+++ obj/Debug/net8.0/AuthDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ac0a631ca346ca52a4620536918597b75605550")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
